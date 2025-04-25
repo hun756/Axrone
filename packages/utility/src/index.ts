d41de084061f023cfe76996d9bef34ca218e845a
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-25 09:33:26 +0300

    feat(utility): add TypedArray type and export from index

---
+++ packages/utility/src/index.ts
@@ -0,0 +1,1 @@
+export { TypedArray } from "./clone/types";
---
+++ packages/utility/src/types.ts
@@ -0,0 +1,14 @@
+export type Primitive = undefined | null | boolean | number | string | bigint | symbol;
+
+export type TypedArray =
+    | Int8Array
+    | Uint8Array
+    | Uint8ClampedArray
+    | Int16Array
+    | Uint16Array
+    | Int32Array
+    | Uint32Array
+    | Float32Array
+    | Float64Array
+    | BigInt64Array
+    | BigUint64Array;
