ed81b764ce84594fd67df5428a85b17066c2c3e5
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-26 08:07:17 +0300

    feat: add IEquatable interface and EquatableBase class for object equality comparison

--- packages/utility/src/comparer/comparer.ts
+++ packages/utility/src/comparer/comparer.ts
@@ -167,3 +167,107 @@ export class ComparerFactory {
         this.comparerMap.set(type, comparerConstructor);
     }
 }
+
+/**
+ * Interface for objects that can be compared for equality.
+ * @template T - The type of the object to be compared.
+ */
+export interface IEquatable<T> {
+    /**
+     * Checks if the current object is equal to another object.
+     * @param other - The object to compare with.
+     * @returns True if the objects are equal, otherwise false.
+     */
+    equals(other: T): boolean;
+}
+
+/**
+ * Base class for equatable objects, providing utility methods for comparison.
+ * @template T - The type of the object to be compared.
+ */
+export abstract class EquatableBase<T> implements IEquatable<T> {
+    abstract equals(other: T): boolean;
+
+    /**
+     * Compares two equatable objects for equality.
+     * @template T - The type of the objects to be compared.
+     * @param a - The first object.
+     * @param b - The second object.
+     * @returns True if the objects are equal, otherwise false.
+     */
+    static areEqual<T extends IEquatable<T>>(a: T, b: T): boolean {
+        if (a === b) return true;
+        if (a === null || b === null) return false;
+        return a.equals(b);
+    }
+
+    /**
+     * Compares two arrays of equatable objects for equality.
+     * @template T - The type of the objects to be compared.
+     * @param a - The first array.
+     * @param b - The second array.
+     * @returns True if the arrays are equal, otherwise false.
+     */
+    static areArraysEqual<T extends IEquatable<T>>(a: T[], b: T[]): boolean {
+        if (a.length !== b.length) return false;
+        for (let i = 0; i < a.length; i++) {
+            if (!a[i].equals(b[i])) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Performs a deep equality check on two objects.
+     * @template T - The type of the objects to be compared.
+     * @param a - The first object.
+     * @param b - The second object.
+     * @returns True if the objects are deeply equal, otherwise false.
+     */
+    static deepEqual<T extends Record<string, any>>(a: T, b: T): boolean {
+        if (a === b) return true;
+        if (typeof a !== 'object' || typeof b !== 'object') return false;
+        if (a === null || b === null) return false;
+
+        const keysA = Object.keys(a);
+        const keysB = Object.keys(b);
+        if (keysA.length !== keysB.length) return false;
+
+        for (const key of keysA) {
+            if (!b.hasOwnProperty(key)) return false;
+            if (!EquatableBase.deepEqual(a[key], b[key])) return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Creates an instance of an equatable object from a JSON string.
+     * @template T - The type of the object to be created.
+     * @param json - The JSON string.
+     * @param ctor - The constructor of the object.
+     * @returns The created object.
+     */
+    static fromJSON<T extends EquatableBase<T>>(json: string, ctor: new () => T): T {
+        const obj = new ctor();
+        Object.assign(obj, JSON.parse(json));
+        return obj;
+    }
+
+    /**
+     * Generates a hash code for an object.
+     * @template T - The type of the object.
+     * @param obj - The object.
+     * @returns The hash code.
+     */
+    static hashCode<T>(obj: T): number {
+        const str = JSON.stringify(obj);
+        let hash = 0;
+        if (str.length === 0) return hash;
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charCodeAt(i);
+            hash = (hash << 5) - hash + char;
+            hash |= 0;
+        }
+        return hash;
+    }
+}
