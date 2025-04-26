53d468ab7bdf7310b810bf1b60b0cf2fad8f5615
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-26 08:02:32 +0300

    feat: enhance ComparerFactory with creation and registration methods for comparers

--- packages/utility/src/comparer/comparer.ts
+++ packages/utility/src/comparer/comparer.ts
@@ -19,7 +19,7 @@ export class NumberComparer implements Comparer<number> {
     /**
      * @param ascending - Determines the sort order. True for ascending, false for descending.
      */
-    constructor(private ascending: boolean = true) { }
+    constructor(private ascending: boolean = true) {}
 
     /**
      * Compares two numbers.
@@ -39,7 +39,7 @@ export class StringComparer implements Comparer<string> {
     /**
      * @param caseSensitive - Determines if the comparison is case-sensitive.
      */
-    constructor(private caseSensitive: boolean = true) { }
+    constructor(private caseSensitive: boolean = true) {}
 
     /**
      * Compares two strings.
@@ -64,7 +64,7 @@ export class CustomComparer<T> implements Comparer<T> {
     /**
      * @param compareFunction - The custom comparison function.
      */
-    constructor(private compareFunction: (a: T, b: T) => number) { }
+    constructor(private compareFunction: (a: T, b: T) => number) {}
 
     /**
      * Compares two values using the custom comparison function.
@@ -100,13 +100,13 @@ export class ObjectPropertyComparer<T> implements Comparer<T> {
     /**
      * @param property - The property of the objects to compare.
      */
-    constructor(private property: keyof T) { }
+    constructor(private property: keyof T) {}
 
     /**
      * Compares two objects based on the specified property.
      * @param a - The first object.
      * @param b - The second object.
-     * @returns A negative number if a[property] < b[property], zero if a[property] = b[property], 
+     * @returns A negative number if a[property] < b[property], zero if a[property] = b[property],
      * a positive number if a[property] > b[property].
      */
     compare(a: T, b: T): number {
@@ -126,3 +126,44 @@ export class ObjectPropertyComparer<T> implements Comparer<T> {
  */
 type ComparerConstructor<T> = new () => Comparer<T>;
 
+/**
+ * Factory class to create comparer instances based on type.
+ */
+export class ComparerFactory {
+    private static comparerMap = new Map<string, ComparerConstructor<any>>([
+        ['number', NumberComparer],
+        ['string', StringComparer],
+        ['date', DateComparer],
+    ]);
+
+    /**
+     * Creates a comparer instance for the specified type or a custom comparer.
+     * @template T - The type of the values to be compared.
+     * @param type - The type of comparer to create.
+     * @param customComparer - Optional custom comparison function.
+     * @returns The comparer instance.
+     * @throws If no comparer is found for the specified type.
+     */
+    static createComparer<T>(type: string, customComparer?: (a: T, b: T) => number): Comparer<T> {
+        if (customComparer) {
+            return new CustomComparer<T>(customComparer);
+        }
+
+        const comparerConstructor = this.comparerMap.get(type);
+        if (!comparerConstructor) {
+            throw new Error(`No comparer found for type: ${type}`);
+        }
+
+        return new comparerConstructor();
+    }
+
+    /**
+     * Registers a new comparer type with its constructor.
+     * @template T - The type of the values to be compared.
+     * @param type - The type key for the comparer.
+     * @param comparerConstructor - The constructor of the comparer.
+     */
+    static registerComparer<T>(type: string, comparerConstructor: ComparerConstructor<T>): void {
+        this.comparerMap.set(type, comparerConstructor);
+    }
+}
