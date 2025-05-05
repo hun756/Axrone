034d9a00269bb45cdaa180fc305f317940edd51d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-05-05 09:50:01 +0300

    feat: extend BuiltinObject type to include additional WebAssembly types

--- packages/utility/src/types.ts
+++ packages/utility/src/types.ts
@@ -26,3 +26,43 @@ export type TypedArrayConstructor =
     | BigInt64ArrayConstructor
     | BigUint64ArrayConstructor;
 
+export type Builtin = Primitive | BuiltinObject;
+
+export type BuiltinObject =
+    | Object
+    | Function
+    | Boolean
+    | Symbol
+    | Error
+    | EvalError
+    | RangeError
+    | ReferenceError
+    | SyntaxError
+    | TypeError
+    | URIError
+    | AggregateError
+    | Number
+    | BigInt
+    | Math
+    | Date
+    | String
+    | RegExp
+    | Array<any>
+    | Map<any, any>
+    | Set<any>
+    | WeakMap<any, any>
+    | WeakSet<any>
+    | Promise<any>
+    | TypedArray
+    | ArrayBuffer
+    | SharedArrayBuffer
+    | DataView
+    | JSON
+    | WebAssembly.Module
+    | WebAssembly.Instance
+    | WebAssembly.Memory
+    | WebAssembly.Table
+    | WebAssembly.Global
+    | WebAssembly.CompileError
+    | WebAssembly.LinkError
+    | WebAssembly.RuntimeError;
