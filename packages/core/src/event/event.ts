88194486cb8e3735711e83e1c385cdbaa304a210
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-06-25 16:46:18 +0300

    feat: add event subscriber, publisher, buffer, observer, and emitter interfaces

--- packages/core/src/event/event.ts
+++ packages/core/src/event/event.ts
@@ -158,3 +158,104 @@ export interface QueuedEvent<T = any> {
     readonly priority: EventPriority;
 }
 
+export interface IEventSubscriber<T extends EventMap = EventMap> {
+    on<K extends EventKey<T>>(
+        event: K,
+        callback: EventCallback<T[K]>,
+        options?: SubscriptionOptions
+    ): UnsubscribeFn;
+
+    once<K extends EventKey<T>>(
+        event: K,
+        callback: EventCallback<T[K]>,
+        options?: Omit<SubscriptionOptions, 'once'>
+    ): UnsubscribeFn;
+
+    off<K extends EventKey<T>>(event: K, callback?: EventCallback<T[K]>): boolean;
+
+    offById(subscriptionId: symbol): boolean;
+
+    pipe<K extends EventKey<T>>(
+        event: K,
+        emitter: IEventPublisher<any>,
+        targetEvent?: string
+    ): UnsubscribeFn;
+}
+
+export interface IEventPublisher<T extends EventMap = EventMap> {
+    emit<K extends EventKey<T>>(
+        event: K,
+        data: T[K],
+        options?: { priority?: EventPriority }
+    ): Promise<boolean>;
+
+    emitSync<K extends EventKey<T>>(
+        event: K,
+        data: T[K],
+        options?: { priority?: EventPriority }
+    ): boolean;
+
+    emitBatch<K extends EventKey<T>>(
+        events: Array<{ event: K; data: T[K]; priority?: EventPriority }>
+    ): Promise<boolean[]>;
+}
+
+export interface IEventBuffer<T extends EventMap = EventMap> {
+    getQueuedEvents<K extends EventKey<T>>(event?: K): ReadonlyArray<QueuedEvent>;
+
+    getPendingCount<K extends EventKey<T>>(event?: K): number;
+
+    getBufferSize(): number;
+
+    clearBuffer<K extends EventKey<T>>(event?: K): number;
+
+    pause(): void;
+
+    resume(): void;
+
+    isPaused(): boolean;
+}
+
+export interface IEventObserver<T extends EventMap = EventMap> {
+    has<K extends EventKey<T>>(event: K): boolean;
+
+    listenerCount<K extends EventKey<T>>(event: K): number;
+
+    maxListeners: number;
+
+    listenerCountAll(): number;
+
+    eventNames(): EventKey<T>[];
+
+    getSubscriptions<K extends EventKey<T>>(event: K): ReadonlyArray<Subscription<T[K]>>;
+
+    hasSubscription(subscriptionId: symbol): boolean;
+
+    getMetrics<K extends EventKey<T>>(event: K): EventMetrics;
+
+    getMemoryUsage(): Record<string, number>;
+}
+
+export interface IEventEmitter<T extends EventMap = EventMap>
+    extends IEventSubscriber<T>,
+        IEventPublisher<T>,
+        IEventObserver<T>,
+        IEventBuffer<T> {
+    removeAllListeners<K extends EventKey<T>>(event?: K): this;
+
+    batchSubscribe<K extends EventKey<T>>(
+        event: K,
+        callbacks: ReadonlyArray<EventCallback<T[K]>>,
+        options?: SubscriptionOptions
+    ): ReadonlyArray<symbol>;
+
+    batchUnsubscribe(subscriptionIds: ReadonlyArray<symbol>): number;
+
+    resetMaxListeners(): void;
+
+    drain(): Promise<void>;
+
+    flush<K extends EventKey<T>>(event: K): Promise<void>;
+
+    resetMetrics<K extends EventKey<T>>(event?: K): void;
+}
