e30344f2947cc2a1f7ecf740710cca879471f77f
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-04 18:34:49 +0300

    feat: Add static create method to Mat4 class with various parameter handling

--- packages/numeric/src/__tests__/mat/mat4.test.ts
+++ packages/numeric/src/__tests__/mat/mat4.test.ts
@@ -153,4 +153,24 @@ describe('Mat4', () => {
             });
         });
     });
+
+    describe('Static create method', () => {
+        test('should create identity matrix with default parameters', () => {
+            const matrix = Mat4.create();
+
+            expect(matrix.data).toEqual([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+        });
+
+        test('should create matrix with all parameters provided', () => {
+            const matrix = Mat4.create(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+            expect(matrix.data).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+        });
+
+        test('should create matrix with partial parameters (rest defaulted)', () => {
+            const matrix = Mat4.create(2, 0, 0, 0, 0, 3);
+
+            expect(matrix.data).toEqual([2, 0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+        });
+    });
 });
