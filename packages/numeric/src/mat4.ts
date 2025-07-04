be3cd16eab1138beb7644df6754d2eb71edced8d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-04 16:31:38 +0300

    refactor: Improve readability of multiply method in Mat4 class

--- packages/numeric/src/mat4.ts
+++ packages/numeric/src/mat4.ts
@@ -167,15 +167,39 @@ export class Mat4 implements IMat4Like, ICloneable<Mat4>, Equatable {
         b: Readonly<U>,
         out?: V
     ): V {
-        const a00 = a.data[0], a01 = a.data[1], a02 = a.data[2], a03 = a.data[3];
-        const a10 = a.data[4], a11 = a.data[5], a12 = a.data[6], a13 = a.data[7];
-        const a20 = a.data[8], a21 = a.data[9], a22 = a.data[10], a23 = a.data[11];
-        const a30 = a.data[12], a31 = a.data[13], a32 = a.data[14], a33 = a.data[15];
+        const a00 = a.data[0],
+            a01 = a.data[1],
+            a02 = a.data[2],
+            a03 = a.data[3];
+        const a10 = a.data[4],
+            a11 = a.data[5],
+            a12 = a.data[6],
+            a13 = a.data[7];
+        const a20 = a.data[8],
+            a21 = a.data[9],
+            a22 = a.data[10],
+            a23 = a.data[11];
+        const a30 = a.data[12],
+            a31 = a.data[13],
+            a32 = a.data[14],
+            a33 = a.data[15];
 
-        const b00 = b.data[0], b01 = b.data[1], b02 = b.data[2], b03 = b.data[3];
-        const b10 = b.data[4], b11 = b.data[5], b12 = b.data[6], b13 = b.data[7];
-        const b20 = b.data[8], b21 = b.data[9], b22 = b.data[10], b23 = b.data[11];
-        const b30 = b.data[12], b31 = b.data[13], b32 = b.data[14], b33 = b.data[15];
+        const b00 = b.data[0],
+            b01 = b.data[1],
+            b02 = b.data[2],
+            b03 = b.data[3];
+        const b10 = b.data[4],
+            b11 = b.data[5],
+            b12 = b.data[6],
+            b13 = b.data[7];
+        const b20 = b.data[8],
+            b21 = b.data[9],
+            b22 = b.data[10],
+            b23 = b.data[11];
+        const b30 = b.data[12],
+            b31 = b.data[13],
+            b32 = b.data[14],
+            b33 = b.data[15];
 
         if (out) {
             (out as any).data[0] = a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30;
@@ -220,8 +244,8 @@ export class Mat4 implements IMat4Like, ICloneable<Mat4>, Equatable {
                     a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30,
                     a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31,
                     a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32,
-                    a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33
-                ]
+                    a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33,
+                ],
             } as unknown as V;
         }
     }
