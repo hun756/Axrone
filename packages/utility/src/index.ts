da7d2cdce6de12d8eeef2162ff160175e533ddd4
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-08-03 16:24:52 +0300

    refactor: Refactor and expand utility module exports

--- packages/utility/src/index.ts
+++ packages/utility/src/index.ts
@@ -1,4 +1,74 @@
-export { TypedArray } from './types';
-export { ICloneable } from './clone/cloner';
-export * from './comparer/comparer';
-export * from './memory';
+export type { Primitive, TypedArray, TypedArrayConstructor, Builtin, BuiltinObject } from './types';
+
+export { ByteBuffer, ByteOrder, SeekOrigin } from './buffering/byte-buffer';
+
+export type {
+    CompareResult,
+    Comparable,
+    OrderKey,
+    Comparer,
+    EqualityComparer,
+    Equatable,
+    KeySelector,
+    PropertyPath,
+    ExtractPropertyType,
+    ComparerOptions,
+    EqualityComparerOptions,
+    DeepPartial,
+    KeysOfType,
+} from './comparer/comparer';
+
+export {
+    DefaultComparer,
+    DefaultEqualityComparer,
+    ReverseComparer,
+    CompositeComparer,
+    KeyComparer,
+    StringComparer,
+    NumberComparer,
+    DateComparer,
+    DeepEqualityComparer,
+    ComparerError,
+    InvalidOperationError,
+    comparer,
+    equality,
+    createOrderKey,
+    createPropertyAccessor,
+    sorted,
+    min,
+    max,
+    isEquatable,
+    isComparer,
+    isEqualityComparer,
+} from './comparer/comparer';
+
+export { PriorityQueue } from './memory/priority-queue';
+export type {
+    Comparator,
+    HeapIndex,
+    QueueSize,
+    Capacity,
+    ReadonlyQueueNode,
+    QueueNode,
+    PriorityQueueOptions,
+    PriorityQueueCore,
+    OptionalOperations,
+    QueryOperations,
+    CapacityOperations,
+} from './memory/priority-queue';
+
+export type {
+    PoolableObject,
+    PoolObjectStatus,
+    PoolExpansionStrategy,
+    PoolAllocationStrategy,
+    PoolEvictionPolicy,
+    MemoryPoolOptions,
+    PoolPerformanceMetrics,
+    MemoryPoolOperations,
+    AsyncMemoryPoolOperations,
+} from './memory/pool/mempool';
+
+export type { ICloneable } from './clone/cloner';
+
+export * from './memory/pool/index'
