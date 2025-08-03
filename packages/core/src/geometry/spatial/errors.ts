7a688acba7f108520740a3295323893be862f781
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-08-03 08:15:05 +0300

    feat: Add spatial error classes and types

---
+++ packages/core/src/geometry/spatial/errors.ts
@@ -0,0 +1,31 @@
+export class SpatialError extends Error {
+    constructor(message: string, public readonly context?: Record<string, unknown>) {
+        super(message);
+        this.name = 'SpatialError';
+        Object.setPrototypeOf(this, SpatialError.prototype);
+    }
+}
+
+export class SpatialBoundsError extends SpatialError {
+    constructor(message: string, context?: Record<string, unknown>) {
+        super(message, context);
+        this.name = 'SpatialBoundsError';
+        Object.setPrototypeOf(this, SpatialBoundsError.prototype);
+    }
+}
+
+export class SpatialItemError extends SpatialError {
+    constructor(message: string, context?: Record<string, unknown>) {
+        super(message, context);
+        this.name = 'SpatialItemError';
+        Object.setPrototypeOf(this, SpatialItemError.prototype);
+    }
+}
+
+export class SpatialConfigError extends SpatialError {
+    constructor(message: string, context?: Record<string, unknown>) {
+        super(message, context);
+        this.name = 'SpatialConfigError';
+        Object.setPrototypeOf(this, SpatialConfigError.prototype);
+    }
+}
