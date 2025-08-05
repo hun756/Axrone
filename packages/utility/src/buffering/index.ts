efa396fcdc2d9a7cbf43599296cd8bfaa64d382c
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-08-05 17:07:15 +0300

    refactor and re-export types in buffering module

--- packages/utility/src/buffering/index.ts
+++ packages/utility/src/buffering/index.ts
@@ -1,13 +1,23 @@
 export * from './types';
-
 export * from './interfaces';
-
 export * from './constants';
-
 export * from './errors';
-
 export * from './buffer-pool';
+
 export { BufferView } from './buffer-view';
 export { BufferUtils } from './utils';
-
 export { ByteBuffer } from './byte-buffer-core';
+
+export type { BufferPoolOptions, BufferPoolStats, BucketStats } from './buffer-pool';
+
+export type { IReadableBuffer, IWritableBuffer, IByteBuffer } from './interfaces';
+
+export type {
+    TypedArrayMap,
+    PrimitiveTypeMap,
+    TypedArrayConstructorMap,
+    ByteOrder,
+    SeekOrigin,
+    BufferState,
+    Nullable,
+} from './types';
