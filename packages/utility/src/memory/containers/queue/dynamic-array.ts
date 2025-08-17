ad452066bd1f0a51dc8d7ac0c7ce2a8510138446
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-08-17 11:50:18 +0300

    refactor: DynamicArray to simplify type handling

--- packages/utility/src/memory/containers/queue/dynamic-array.ts
+++ packages/utility/src/memory/containers/queue/dynamic-array.ts
@@ -1,88 +1,111 @@
 import { HeapIndex, QueueSize, Capacity, HeapStorage } from './types';
 import { EmptyQueueError, InvalidCapacityError } from './errors';
-import { createQueueSize, createCapacity } from './utils';
 
 export class DynamicArray<T> implements HeapStorage<T> {
-    private buffer: Array<T>;
-    private _length: QueueSize;
-    private _capacity: Capacity;
+    private buffer: T[];
+    private _length: number;
+    private _capacity: number;
 
-    constructor(initialCapacity: Capacity = createCapacity(16)) {
-        this._capacity = initialCapacity;
-        this._length = createQueueSize(0);
-        this.buffer = new Array(initialCapacity as unknown as number);
+    constructor(initialCapacity?: Capacity) {
+        const capacity = initialCapacity != null ? (initialCapacity as number) : 16;
+
+        if (capacity < 0) {
+            throw new InvalidCapacityError(capacity);
+        }
+
+        this._capacity = capacity;
+        this._length = 0;
+
+        this.buffer = new Array<T>(capacity);
     }
 
     get length(): QueueSize {
-        return this._length;
+        return this._length as QueueSize;
     }
 
     get capacity(): Capacity {
-        return this._capacity;
+        return this._capacity as Capacity;
     }
 
     get(index: HeapIndex): T {
-        return this.buffer[index as unknown as number];
+        return this.buffer[index as number];
     }
 
     set(index: HeapIndex, value: T): void {
-        this.buffer[index as unknown as number] = value;
+        this.buffer[index as number] = value;
     }
 
     push(value: T): void {
-        this.ensureCapacity(createCapacity((this._length as unknown as number) + 1));
-        this.buffer[this._length as unknown as number] = value;
-        this._length = createQueueSize((this._length as unknown as number) + 1);
+        if (this._length >= this._capacity) {
+            this.grow();
+        }
+
+        this.buffer[this._length] = value;
+        this._length++;
     }
 
     pop(): T {
-        if ((this._length as unknown as number) === 0) {
+        if (this._length === 0) {
             throw new EmptyQueueError();
         }
-        this._length = createQueueSize((this._length as unknown as number) - 1);
-        return this.buffer[this._length as unknown as number];
+
+        this._length--;
+        return this.buffer[this._length];
     }
 
     swap(i: HeapIndex, j: HeapIndex): void {
-        const ii = i as unknown as number;
-        const jj = j as unknown as number;
-        const temp = this.buffer[ii];
-        this.buffer[ii] = this.buffer[jj];
-        this.buffer[jj] = temp;
+        const buffer = this.buffer;
+        const iIndex = i as number;
+        const jIndex = j as number;
+
+        const temp = buffer[iIndex];
+        buffer[iIndex] = buffer[jIndex];
+        buffer[jIndex] = temp;
     }
 
     resize(newCapacity: Capacity): void {
-        if ((newCapacity as unknown as number) < (this._length as unknown as number)) {
-            throw new InvalidCapacityError(newCapacity as unknown as number);
+        const newCap = newCapacity as number;
+
+        if (newCap < this._length) {
+            throw new InvalidCapacityError(newCap);
         }
 
-        const newBuffer = new Array<T>(newCapacity as unknown as number);
-        for (let i = 0; i < (this._length as unknown as number); i++) {
+        const newBuffer = new Array<T>(newCap);
+
+        for (let i = 0; i < this._length; i++) {
             newBuffer[i] = this.buffer[i];
         }
 
         this.buffer = newBuffer;
-        this._capacity = newCapacity;
+        this._capacity = newCap;
     }
 
     ensureCapacity(minCapacity: Capacity): void {
-        if ((minCapacity as unknown as number) > (this._capacity as unknown as number)) {
-            const newCapacity = createCapacity(Math.max(minCapacity as unknown as number, (this._capacity as unknown as number) * 2));
-            this.resize(newCapacity);
+        const minCap = minCapacity as number;
+
+        if (minCap > this._capacity) {
+            const newCapacity = Math.max(minCap, this._capacity << 1);
+            this.resize(newCapacity as Capacity);
         }
     }
 
     trimToSize(): void {
-        if ((this._length as unknown as number) < (this._capacity as unknown as number)) {
-            this.resize(createCapacity(Math.max(1, this._length as unknown as number)));
+        if (this._length < this._capacity) {
+            const newCapacity = Math.max(1, this._length);
+            this.resize(newCapacity as Capacity);
         }
     }
 
     clear(): void {
-        this._length = createQueueSize(0);
+        this._length = 0;
     }
 
     slice(): T[] {
-        return this.buffer.slice(0, this._length as unknown as number);
+        return this.buffer.slice(0, this._length);
+    }
+
+    private grow(): void {
+        const newCapacity = Math.max(this._capacity << 1, this._capacity + 1);
+        this.resize(newCapacity as Capacity);
     }
 }
