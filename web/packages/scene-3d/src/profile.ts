ab3b4671e48a2cb59252f3e0afa6aadbd39f9a98
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-05-15 07:43:29 +0300

    refactor: scene-3d public API re-exports

---
+++ web/packages/scene-3d/src/profile.ts
@@ -0,0 +1,14 @@
+export type {
+    SceneManifestRuntimeProfileOptions,
+    SceneRuntimeProfile,
+    SceneRuntimeProfileContext,
+} from '@axrone/scene-runtime/scene-profile';
+export {
+    DEFAULT_SCENE_RUNTIME_PROFILE_ID,
+    SCENE_3D_RUNTIME_PROFILE_ID,
+    createSceneManifestRuntimeProfile,
+    createSceneRuntimeProfile,
+    get3DSceneRuntimeProfile,
+    getDefaultSceneRuntimeProfile,
+    resolveSceneRegistryFromProfile,
+} from '@axrone/scene-runtime/scene-profile';
---
+++ web/packages/scene-3d/src/types.ts
@@ -0,0 +1,11 @@
+export type * from '@axrone/scene-runtime';
+export type {
+    SceneManifestRuntimeProfileOptions,
+    SceneRuntimeProfile,
+    SceneRuntimeProfileContext,
+} from '@axrone/scene-runtime/scene-profile';
+export type {
+    Scene3DActorRuntimeOptions,
+    SceneRenderableActorCreateOptions,
+    SceneRenderableActorInstance,
+} from './scene-3d-actor-runtime';
