a288c772fbd0fb1076c5389e5f14cb3409b96ba4
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-07-08 14:07:28 +0300

    feat(asset-shader): expand exports to include shader library and variant functionalities

--- web/packages/asset-shader/src/index.ts
+++ web/packages/asset-shader/src/index.ts
@@ -40,6 +40,69 @@ export {
     type ShaderEffectPropertyOptions,
     type ShaderEffectStageOptions,
 } from './authoring';
+export {
+    enumProp,
+    extend,
+    keyword,
+    library,
+    pass,
+    rangeProp,
+    reflect,
+    technique,
+    toggleProp,
+    type RenderShaderPassDefinitionInput,
+} from './authoring';
+export {
+    type ShaderDiagnostic,
+    type ShaderDiagnosticLocation,
+    type ShaderDiagnosticSeverity,
+    type ShaderDiagnosticSink,
+    createDiagnosticSink,
+    formatShaderDiagnostic,
+} from './diagnostics';
+export {
+    type ShaderPreprocessOptions,
+    type ShaderPreprocessResult,
+    ShaderPreprocessError,
+    preprocessGLSL,
+} from './preprocessor';
+export {
+    type ShaderIncludeOptions,
+    type ShaderIncludeResult,
+    type ShaderLibraryEntry,
+    clearShaderLibraries,
+    defineShaderLibrary,
+    expandShaderIncludes,
+    getShaderLibrary,
+    hasShaderLibrary,
+    listShaderLibraries,
+    registerShaderLibrary,
+    resolveShaderLibraries,
+} from './library';
+export {
+    type BuildShaderVariantOptions,
+    type ShaderDefineMap,
+    type ShaderKeywordSelection,
+    type ShaderVariant,
+    buildShaderVariant,
+    buildShaderVariants,
+    defaultShaderKeywordSelection,
+    enumerateShaderVariants,
+    selectionToShaderDefines,
+    shaderVariantCount,
+    shaderVariantKey,
+} from './variants';
+export { extendShaderEffect, type PartialShaderEffectOverride } from './compose';
+export {
+    type ShaderAttributeReflection,
+    type ShaderEffectReflection,
+    type ShaderKeywordReflection,
+    type ShaderStageReflection,
+    type ShaderTechniquePassReflection,
+    type ShaderTechniqueReflection,
+    type ShaderUniformReflection,
+    reflectShaderEffect,
+} from './reflection';
 
 export type {
     CompiledRenderShaderEffect,
