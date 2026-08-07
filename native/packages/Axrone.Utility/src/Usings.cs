5c29b1a9c24c6550e5f496c2633542c984b81c9c
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-08-07 21:48:00 +0300

    chore(singleton): wire singleton namespaces into global usings

--- native/packages/Axrone.Utility/src/Usings.cs
+++ native/packages/Axrone.Utility/src/Usings.cs
@@ -16,6 +16,14 @@ global using Axrone.Utility.Base64;
 
 // Singleton
 global using Axrone.Utility.Singleton;
+global using Axrone.Utility.Singleton.Internal;
+
+// Runtime
+global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Collections.Concurrent;
 
 // Message catalog
 global using Axrone.Utility.MessageCatalog;
