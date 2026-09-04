d6e02db1c2893ec624ea28fd7773e943838b016e
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-04 18:22:17 +0300

    feat(lifetime): add value-type singleton handle for implicit resolution

---
+++ native/packages/Axrone.Memory/src/Lifetime/SingletonHandle.cs
@@ -0,0 +1,35 @@
+namespace Enterprise.Patterns.Singleton;
+
+public readonly struct SingletonHandle<T> : IEquatable<SingletonHandle<T>> where T : class
+{
+    private readonly ISingleton<T> _singleton;
+
+    public SingletonHandle(ISingleton<T> singleton)
+    {
+        ArgumentNullException.ThrowIfNull(singleton);
+        _singleton = singleton;
+    }
+
+    public T Value
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _singleton.Value;
+    }
+
+    public bool IsCreated => _singleton.IsValueCreated;
+
+    public SingletonLifecycleState State => _singleton.State;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator T(SingletonHandle<T> handle) => handle.Value;
+
+    public bool Equals(SingletonHandle<T> other) => ReferenceEquals(_singleton, other._singleton);
+
+    public override bool Equals(object? obj) => obj is SingletonHandle<T> other && Equals(other);
+
+    public override int GetHashCode() => _singleton?.GetHashCode() ?? 0;
+
+    public static bool operator ==(SingletonHandle<T> left, SingletonHandle<T> right) => left.Equals(right);
+
+    public static bool operator !=(SingletonHandle<T> left, SingletonHandle<T> right) => !left.Equals(right);
+}
