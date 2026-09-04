71ed831c853ba117fd3115f0dae97dfb3a227b7b
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-04 18:23:28 +0300

    feat(lifetime): add registry extension methods for typed handle resolution

---
+++ native/packages/Axrone.Memory/src/Lifetime/SingletonRegistryExtensions.cs
@@ -0,0 +1,30 @@
+namespace Enterprise.Patterns.Singleton;
+
+public static class SingletonRegistryExtensions
+{
+    public static SingletonHandle<T> GetHandle<T>(this ISingletonRegistry registry) where T : class
+    {
+        ArgumentNullException.ThrowIfNull(registry);
+        return new SingletonHandle<T>(new RegistrySingletonProxy<T>(registry));
+    }
+
+    private sealed class RegistrySingletonProxy<T> : ISingleton<T> where T : class
+    {
+        private readonly ISingletonRegistry _registry;
+
+        public RegistrySingletonProxy(ISingletonRegistry registry)
+        {
+            _registry = registry;
+        }
+
+        public T Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _registry.Get<T>();
+        }
+
+        public bool IsValueCreated => _registry.TryGet<T>(out _);
+
+        public SingletonLifecycleState State => _registry.Contains<T>() ? SingletonLifecycleState.Initialized : SingletonLifecycleState.Uninitialized;
+    }
+}
