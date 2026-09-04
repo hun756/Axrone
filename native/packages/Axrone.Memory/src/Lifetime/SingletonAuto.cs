e383a0c0e7a4758e02cb57e09be6b16c9a120421
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-04 18:21:12 +0300

    feat(lifetime): add auto-initializing singleton for parameterless types

---
+++ native/packages/Axrone.Memory/src/Lifetime/SingletonAuto.cs
@@ -0,0 +1,14 @@
+namespace Enterprise.Patterns.Singleton;
+
+public static class SingletonAuto<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] T> where T : class, new()
+{
+    private static readonly Lazy<T> s_lazy = new(static () => new T(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+    public static T Instance
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => s_lazy.Value;
+    }
+
+    public static bool IsValueCreated => s_lazy.IsValueCreated;
+}
