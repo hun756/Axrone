2af70f204c564d37867a597822e6aded0367f4db
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-07 18:54:41 +0300

    feat(physics-2d): add wheel joint component

--- web/packages/physics-2d/src/components/index.ts
+++ web/packages/physics-2d/src/components/index.ts
@@ -10,4 +10,5 @@ export * from './hinge-joint2d';
 export * from './slider-joint2d';
 export * from './spring-joint2d';
 export * from './fixed-joint2d';
+export * from './wheel-joint2d';
 export * from './physics-world-2d-component';
