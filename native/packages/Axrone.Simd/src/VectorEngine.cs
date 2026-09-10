a35b069cfef6cad11e247528e2ee1c68ba7b627d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-10 21:23:18 +0300

    refactor(simd): use shared BatchCapacity, MemoryAlignment and AlignedCounter from Utility

--- native/packages/Axrone.Simd/src/VectorEngine.cs
+++ native/packages/Axrone.Simd/src/VectorEngine.cs
@@ -2,58 +2,6 @@
 
 namespace Axrone.Simd;
 
-public sealed unsafe class AlignedCounter : IDisposable
-{
-    private long* _value;
-    private int _disposed;
-
-    public long Value
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => Interlocked.Read(ref *_value);
-    }
-
-    public AlignedCounter(long initialValue = 0)
-    {
-        _value = (long*)NativeMemory.AlignedAlloc(128, 128);
-        NativeMemory.Clear(_value, 128);
-        *_value = initialValue;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public long Increment() => Interlocked.Increment(ref *_value);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public long Add(long value) => Interlocked.Add(ref *_value, value);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool CompareExchange(long expected, long next) => Interlocked.CompareExchange(ref *_value, next, expected) == expected;
-
-    ~AlignedCounter()
-    {
-        Release();
-    }
-
-    public void Dispose()
-    {
-        Release();
-        GC.SuppressFinalize(this);
-    }
-
-    private void Release()
-    {
-        if (Interlocked.Exchange(ref _disposed, 1) == 0)
-        {
-            long* ptr = _value;
-            _value = null;
-            if (ptr != null)
-            {
-                NativeMemory.AlignedFree(ptr);
-            }
-        }
-    }
-}
-
 public sealed class Float32VectorEngine : IVectorTransformer<float>, IVectorReducer<float>, IVectorScanner<float>, IDisposable
 {
     private readonly AlignedCounter _transformsExecuted = new();
