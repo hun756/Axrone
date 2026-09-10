7d0b60ea69276bfdf1e65069ce89b1236700334d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-10 21:10:08 +0300

    refactor(simd): remove dead IContentionStrategy copy from VectorEngine

--- native/packages/Axrone.Simd/src/VectorEngine.cs
+++ native/packages/Axrone.Simd/src/VectorEngine.cs
@@ -2,44 +2,6 @@
 
 namespace Axrone.Simd;
 
-public interface IContentionStrategy
-{
-    static abstract void Backoff(uint iteration);
-}
-
-public readonly struct AdaptiveBackoff : IContentionStrategy
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    public static void Backoff(uint iteration)
-    {
-        if (iteration < 4)
-        {
-            Thread.SpinWait(1 << (int)iteration);
-        }
-        else if (iteration < 16)
-        {
-            Thread.SpinWait(1 << 5);
-        }
-        else if (iteration < 32)
-        {
-            Thread.Yield();
-        }
-        else
-        {
-            Thread.Sleep(0);
-        }
-    }
-}
-
-public readonly struct PinnedCoreSpinBackoff : IContentionStrategy
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Backoff(uint iteration)
-    {
-        Thread.SpinWait(Math.Min((int)iteration + 1, 32));
-    }
-}
-
 public sealed unsafe class AlignedCounter : IDisposable
 {
     private long* _value;
