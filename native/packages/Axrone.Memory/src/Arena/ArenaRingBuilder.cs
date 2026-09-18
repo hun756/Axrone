cfc1f6b7ded460764c5ff50e40414a58bcbe69f5
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-18 11:53:59 +0300

    feat(memory): add fluent ArenaRingBuilder for constructing configured arena ring instances

---
+++ native/packages/Axrone.Memory/src/Arena/ArenaRingBuilder.cs
@@ -0,0 +1,81 @@
+using Axrone.Utility.Alignment;
+
+namespace Axrone.Memory.Arena;
+
+public sealed class ArenaRingBuilder<T> where T : unmanaged
+{
+    private nuint _capacity;
+    private MemoryTopology _topology = MemoryTopology.NativeAligned;
+    private Alignment _alignment;
+    private bool _zeroOnRecycle;
+    private string _meterName = "Axrone.Memory.Arena";
+    private string _instanceName = "default";
+
+    public ArenaRingBuilder(nuint capacity)
+    {
+        _capacity = capacity;
+    }
+
+    public static ArenaRingBuilder<T> Create(nuint capacity) => new(capacity);
+
+    public ArenaRingBuilder<T> WithCapacity(nuint capacity)
+    {
+        _capacity = capacity;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithTopology(MemoryTopology topology)
+    {
+        _topology = topology;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithNativeAlignedStorage()
+    {
+        _topology = MemoryTopology.NativeAligned;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithPinnedObjectHeapStorage()
+    {
+        _topology = MemoryTopology.PinnedObjectHeap;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithAlignment(Alignment alignment)
+    {
+        _alignment = alignment;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithZeroOnRecycle(bool value = true)
+    {
+        _zeroOnRecycle = value;
+        return this;
+    }
+
+    public ArenaRingBuilder<T> WithTelemetry(string meterName, string instanceName = "default")
+    {
+        _meterName = meterName;
+        _instanceName = instanceName;
+        return this;
+    }
+
+    public ArenaMemoryRing<T, TBackoff> Build<TBackoff>()
+        where TBackoff : struct, IBackoffPolicy
+    {
+        BufferCapacity capacity = new(_capacity);
+        return new ArenaMemoryRing<T, TBackoff>(
+            capacity,
+            _topology,
+            _alignment,
+            _zeroOnRecycle,
+            _meterName,
+            _instanceName);
+    }
+
+    public ArenaMemoryRing<T, ProgressiveBackoff> Build()
+    {
+        return Build<ProgressiveBackoff>();
+    }
+}
