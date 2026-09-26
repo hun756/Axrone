9ced947eb549badc7b776aafc43891a723a14b6a
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-26 19:38:08 +0300

    test(render): wire mock backend into test project

--- native/packages/Axrone.Render.OpenGL/tests/GlobalUsings.cs
+++ native/packages/Axrone.Render.OpenGL/tests/GlobalUsings.cs
@@ -2,6 +2,7 @@ global using System;
 global using System.Numerics;
 global using Axrone.Render.Core;
 global using Axrone.Render.OpenGL;
+global using Axrone.Render.OpenGL.Mock;
 global using Axrone.Render.OpenGL.Texture;
 global using Xunit;
 global using FluentAssertions;
