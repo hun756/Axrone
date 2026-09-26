ad4ad5bb80fe5111f7a2492b9f18224b3d902158
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-26 18:27:53 +0300

    test(render): cover coded errors and throw matrix

---
+++ native/packages/Axrone.Render.Core/tests/ErrorTests.cs
@@ -0,0 +1,44 @@
+namespace Axrone.Render.Core.Tests;
+
+using Xunit;
+using FluentAssertions;
+
+public class ErrorTests
+{
+    [Fact]
+    public void CodedExceptions_CarryCodes()
+    {
+        var render = new RenderException("boom", RenderErrorCode.BackendFailed);
+        render.Code.Should().Be(RenderErrorCode.BackendFailed);
+
+        var gl = new GLException("lost", GLContextErrorCode.ContextLost);
+        gl.Code.Should().Be(GLContextErrorCode.ContextLost);
+
+        var buffer = new GLBufferException("bounds", GLBufferErrorCode.BoundsExceeded);
+        buffer.BufferCode.Should().Be(GLBufferErrorCode.BoundsExceeded);
+
+        var shader = new GLShaderException("compile", GLShaderErrorCode.ShaderCompileFailed, "log");
+        shader.ShaderCode.Should().Be(GLShaderErrorCode.ShaderCompileFailed);
+        shader.InfoLog.Should().Be("log");
+
+        var framebuffer = new GLFramebufferException("incomplete", GLFramebufferErrorCode.IncompleteFramebuffer, 0x8CDD);
+        framebuffer.FramebufferCode.Should().Be(GLFramebufferErrorCode.IncompleteFramebuffer);
+        framebuffer.Status.Should().Be(0x8CDD);
+    }
+
+    [Fact]
+    public void ThrowHelper_DoesNotReturn()
+    {
+        Action contextLost = () => ThrowHelper.ThrowContextLost();
+        contextLost.Should().Throw<GLException>()
+            .Where(ex => ex.Code == GLContextErrorCode.ContextLost);
+
+        Action bounds = () => ThrowHelper.ThrowBufferBoundsExceeded();
+        bounds.Should().Throw<GLBufferException>()
+            .Where(ex => ex.BufferCode == GLBufferErrorCode.BoundsExceeded);
+
+        Action executor = () => ThrowHelper.ThrowExecutorNotFound("Shadow");
+        executor.Should().Throw<RenderException>()
+            .Where(ex => ex.Code == RenderErrorCode.ExecutorNotFound);
+    }
+}
