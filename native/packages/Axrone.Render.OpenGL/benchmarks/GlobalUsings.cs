a0f3cdc4389eca59dff7e9800761debece4bb39d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-26 19:53:28 +0300

    feat(render): scaffold benchmark harness

---
+++ native/packages/Axrone.Render.OpenGL/benchmarks/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Numerics;
+global using System.Runtime.CompilerServices;
+global using BenchmarkDotNet.Attributes;
+global using BenchmarkDotNet.Running;
+global using Axrone.Render.Core;
+global using Axrone.Render.OpenGL;
+global using Axrone.Render.OpenGL.Mock;
