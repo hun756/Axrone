32cc4ca178468ff162b3ca209863506f93aee0f7
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-09-26 18:30:37 +0300

    test(render): cover buffer math

---
+++ native/packages/Axrone.Render.Core/tests/FormatMathTests.cs
@@ -0,0 +1,19 @@
+namespace Axrone.Render.Core.Tests;
+
+using Xunit;
+using FluentAssertions;
+
+public class FormatMathTests
+{
+    [Fact]
+    public void BufferMath_AlignsUp()
+    {
+        BufferMath.AlignTo(0, 16).Should().Be(0);
+        BufferMath.AlignTo(1, 16).Should().Be(16);
+        BufferMath.AlignTo(16, 16).Should().Be(16);
+        BufferMath.AlignTo(17, 16).Should().Be(32);
+        BufferMath.IsAligned(32, 16).Should().BeTrue();
+        BufferMath.IsPowerOf2(16).Should().BeTrue();
+        BufferMath.NextPowerOf2(17).Should().Be(32);
+    }
+}
